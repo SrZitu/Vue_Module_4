b51cf0581320bc2a7bfabe2dd918077b68f94e2f
SrZitu <zitu094@gmail.com>
2023-07-22 11:46:02 +0600

    to make  object reactive, use reactive

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <script setup>
-import { ref } from 'vue';
-let location=ref("Dhaka");
-function changeLocation(newLocation){
-location.value=newLocation;
+import { ref,reactive } from 'vue';
+let location=reactive({
+  name:"Dhaka",
+  wheather:"sunny",
+
+});
+
+function changeLocation(newLocation,newwheather){
+location.name=newLocation;
+location.wheather=newwheather;
+
 }
 </script>
 
@@ -10,18 +17,18 @@ location.value=newLocation;
  <section class="container mx-auto flex items-center flex-col">
         <h1 class="text-center text-2xl py-10">Events & Reactivity in Vue.js</h1>
         <div class="container mx-auto flex space-x-5 justify-center m-5">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Dhaka')">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Dhaka','Sunny')">
                 Dhaka
             </button>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Rashahi')">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Rashahi','Cloudy')">
                 Rajshahi
             </button>
-              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Khulna')">
+              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Khulna','Thunder')">
                 Khulna
             </button>
             
         </div>
-        <h1 class="text-2xl m-10">Current Location is {{ location }}</h1>
+        <h1 class="text-2xl m-10 text-blue-500">Current Location is {{ location.name }} <br/>Wheather:{{ location.wheather }} </h1>
     </section>
 </template>
 
