ed4bdde59d24cd58e2ca47c21c5f3b53931a6dfd
SrZitu <zitu094@gmail.com>
2023-07-22 13:21:19 +0600

    Reactive episode 04, reactive array,toggling accordion

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <script setup>
-import { ref } from 'vue'
-let accordionIndex=ref(0);
+import { reactive, ref } from 'vue'
+let accordions=reactive([
+  false,false,false,false
+])
+
 let icons={
   plus:'fas fa-plus',
   minus:'fas fa-minus'
 }
+function toggle(index){
+accordions[index]=!accordions[index]
+}
 </script>
+
 
 <template>
   <section class="container mx-auto flex items-center flex-col">
     <h1 class="text-center text-2xl py-10">Events & Reactivity in Vue.js</h1>
 
-    <div class="p-10 bg-gradient-to-br from-indigo-100 to-indigo-300 grid place-items-center">
-      <div class="w-6/12 mx-auto rounded border">
+    <div class="w-full p-10 bg-gradient-to-br from-pink-50 to-indigo-100 grid place-items-center">
+      <div class="w-3/4 mx-auto rounded border">
         <div class="bg-white p-10 shadow-sm">
           <h3 class="text-lg font-medium text-gray-800">Several Windows stacked on each other</h3>
           <p class="text-sm font-light text-gray-600 my-3">
@@ -24,12 +31,12 @@ let icons={
           <!-- What is term -->
           <div class="transition hover:bg-indigo-50">
             <!-- header -->
-            <div @click="accordionIndex=0" class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
-              <i  :class="accordionIndex===0?[icons.minus,'text-red-600']:[icons.plus,'text-green-600']"></i>
+            <div @click="toggle(0)" class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
+              <i  :class="accordions[0]?[icons.minus,'text-red-600']:[icons.plus,'text-green-600']"></i>
               <h3>What is term?</h3>
             </div>
             <!-- Content -->
-            <div class="px-5 pt-0 text-left pb-5" v-show="accordionIndex==0" >
+            <div class="px-5 pt-0 text-left pb-5" v-show="accordions[0]">
               <p class="leading-6 font-light pl-9 ">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi dolor dolorum odio, saepe quibusdam iusto possimus nesciunt dolores assumenda quae totam, doloremque odit. Itaque cum animi, labore debitis deserunt iusto!
               </p>
@@ -39,12 +46,12 @@ let icons={
           <!-- When to use Accordion Components -->
           <div class="transition hover:bg-indigo-50">
             <!-- header -->
-            <div @click="accordionIndex=1"  class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
-              <i  :class="accordionIndex===1?[icons.minus,'text-red-600']:[icons.plus,'text-green-600']"></i>
+            <div @click="accordions[1]=!accordions[1]" class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
+              <i  :class="accordions[1]?[icons.minus,'text-red-600']:[icons.plus,'text-green-600']"></i>
               <h3>When to use Accordion Components?</h3>
             </div>
             <!-- Content -->
-            <div class="accordion-content px-5 pt-0 text-left pb-5" v-show="accordionIndex==1">
+            <div class="accordion-content px-5 pt-0 text-left pb-5" v-show="accordions[1]">
               <p class="leading-6 font-light pl-9 ">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut suscipit itaque, sequi incidunt ab sit optio debitis dignissimos doloribus ratione nobis id mollitia maiores eveniet necessitatibus dolorum praesentium! Corrupti, at.
               </p>
@@ -54,12 +61,12 @@ let icons={
           <!-- Accordion Wrapper -->
           <div class="transition hover:bg-indigo-50">
             <!-- header -->
-            <div @click="accordionIndex=2" class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
-              <i  :class="accordionIndex===2?[icons.minus,'text-red-600']:[icons.plus,'text-green-600']"></i>
+            <div @click="toggle(2)" class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
+              <i  :class="accordions[2]?[icons.minus,'text-red-600']:[icons.plus,'text-green-600']"></i>
               <h3>How can it be defined?</h3>
             </div>
             <!-- Content -->
-            <div class="accordion-content px-5 pt-0 text-left pb-5" v-show="accordionIndex==2" >
+            <div class="accordion-content px-5 pt-0 text-left pb-5" v-show="accordions[2]">
               <p class="leading-6 font-light pl-9">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima facilis labore voluptates provident nam, delectus suscipit nihil optio voluptate quae porro fugiat magni excepturi doloribus.
               </p>
@@ -69,12 +76,12 @@ let icons={
           <!-- Accordion Wrapper -->
           <div class="transition hover:bg-indigo-50">
             <!-- header -->
-            <div @click="accordionIndex=3"  class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
-              <i  :class="accordionIndex===3?[icons.minus,'text-red-600']:[icons.plus,'text-green-600']"></i>
+            <div @click="toggle(3)" class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
+              <i  :class="accordions[3]?[icons.minus,'text-red-600']:[icons.plus,'text-green-600']"></i>
               <h3>Chamber reached do he nothing be?</h3>
             </div>
             <!-- Content -->
-            <div class="accordion-content px-5 pt-0 text-left pb-5" v-show="accordionIndex==3" >
+            <div class="accordion-content px-5 pt-0 text-left pb-5" v-show="accordions[3]">
               <p class="leading-6 font-light pl-9">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam minima maiores magni dolore qui architecto pariatur incidunt repellat quos hic? Eius, eveniet accusantium! Quisquam, quo. Vitae ipsum ad veritatis commodi quasi expedita!
               </p>
@@ -83,8 +90,6 @@ let icons={
         </div>
       </div>
     </div>
-
   </section>
 </template>
-
 <style scoped></style>
