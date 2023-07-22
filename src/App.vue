ebe118d206dfa8cd7690e97366ddd32cd177185a
SrZitu <zitu094@gmail.com>
2023-07-22 11:17:35 +0600

    event and reactivity episode-1

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <script setup>
-const icons = [
-  { title: "Sunny", icon: "https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911203/weather-icon-md.png" },
-  { title: "Partly Cloudy", icon: "https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912718/weather-icon-md.png" },
-  { title: "Rainy", icon: "https://creazilla-store.fra1.digitaloceanspaces.com/icons/7913380/weather-icon-md.png" },
-  { title: "Thunderstorm", icon: "https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912589/weather-icon-md.png" },
-];
+import { ref } from 'vue';
+let location=ref("Dhaka");
+function changeLocation(newLocation){
+location.value=newLocation;
+}
 </script>
 
 <template>
-  <section class="mx-auto container">
-    <section class="mx-auto container space-y-3 p-3 border border-gray-500 w-80 flex flex-col items-center">
-      <template v-for="icon in icons" :key="icon.icon">
-        <h2 class="text-2xl">{{ icon.title }}</h2>
-        <img class="w-32" :src="icon.icon" :alt="icon.title">
-      </template>
+ <section class="container mx-auto flex items-center flex-col">
+        <h1 class="text-center text-2xl py-10">Events & Reactivity in Vue.js</h1>
+        <div class="container mx-auto flex space-x-5 justify-center m-5">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Dhaka')">
+                Dhaka
+            </button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Rashahi')">
+                Rajshahi
+            </button>
+              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Khulna')">
+                Khulna
+            </button>
+            
+        </div>
+        <h1 class="text-2xl m-10">Current Location is {{ location }}</h1>
     </section>
-  </section>
 </template>
 
 <style scoped></style>
